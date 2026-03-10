d0cf41d19e222f37ebcdd5cf45eb721be14e4a94
Alik Volunets <alikvolynez@mail.ru>
2026-03-10 18:04:56 +0300

    2

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,7 +1,10 @@
 import { rules, createComparison } from '../lib/compare.js';
 
+
 export function initSearching(searchField) {
+  
   // @todo: #5.1 — настроить компаратор
+  
   const compare = createComparison(
     ['skipEmptyTargetValues'],
     [
@@ -14,7 +17,9 @@ export function initSearching(searchField) {
   );
 
   return (data, state, action) => {
+    
     // @todo: #5.2 — применить компаратор
+    
     return data.filter((row) => compare(row, state));
   };
-}          
+}        
