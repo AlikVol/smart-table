51f7fd5cf275393ea27708c58f63c81800fe506e
Alik Volunets <alikvolynez@mail.ru>
2026-03-10 17:43:01 +0300

    1

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
-
+    import { initPagination } from './components/pagination.js';
+    import { initSorting } from './components/sorting.js';
+    import { initFiltering } from './components/filtering.js';
+    import { initSearching } from './components/searching.js';
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -19,9 +22,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,10 +40,12 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-
-
-    sampleTable.render(result)
-}
+        result = applySearching(result, state, action);
+        result = applyFiltering(result, state, action);
+        result = applySorting(result, state, action);
+        result = applyPagination(result, state, action);
+        sampleTable.render(result)
+        }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
@@ -46,8 +55,22 @@ const sampleTable = initTable({
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+  searchBySeller: indexes.sellers,
+});
 
-
+const applySearching = initSearching('search');
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
